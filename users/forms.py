cc546322d5112319939398508850168a407cdf24
itOmelchDO <it.omelchenko@gmail.com>
2023-04-19 02:15:52 +0300

    'Added registration form to app "users"'

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -15,3 +15,28 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ("username", "password")
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control py-4",
+        "placeholder": "Введіть ім'я"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control py-4",
+        "placeholder": "Введіть прізвище"}))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control py-4",
+        "placeholder": "Введіть ім'я користувача"}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        "class": "form-control py-4",
+        "placeholder": "Введіть адресу електронної пошти"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        "class": "form-control py-4",
+        "placeholder": "Введіть пароль"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        "class": "form-control py-4",
+        "placeholder": "Підтвердіть пароль"}))
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "email", "password1", "password2")
