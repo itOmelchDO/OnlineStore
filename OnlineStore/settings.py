b1c0372174398f3bf6150ec19763a9e38cb9b8b9
itOmelchDO <it.omelchenko@gmail.com>
2023-04-25 22:38:02 +0300

    'Connected google service to send emails'

--- OnlineStore/settings.py
+++ OnlineStore/settings.py
@@ -143,5 +143,12 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 # Sending emails
+
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = "test.onlinestore.test@gmail.com"
+EMAIL_HOST_PASSWORD = "Dima1712"
 
