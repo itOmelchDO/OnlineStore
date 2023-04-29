40f9635ebde40aa1623c0728eb0a4e9abc2b6a02
itOmelchDO <it.omelchenko@gmail.com>
2023-04-30 01:24:03 +0300

    'Added python-dotenv to requirements.txt & fixed settings.py'

--- OnlineStore/settings.py
+++ OnlineStore/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,12 +81,14 @@ WSGI_APPLICATION = "OnlineStore.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+load_dotenv()
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "store_db",
-        "USER": "develop",
-        "PASSWORD": "develop",
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         "HOST": "localhost",
         "PORT": "5432",
     }
