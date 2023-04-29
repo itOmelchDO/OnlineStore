c433ca4238b5974541aa07400762bdf410161a35
itOmelchDO <it.omelchenko@gmail.com>
2023-04-30 00:42:19 +0300

    'Connected db to Postgresql'

--- OnlineStore/settings.py
+++ OnlineStore/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "OnlineStore.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "store_db",
+        "USER": "itomelch",
+        "PASSWORD": "Dima1712Diana",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
