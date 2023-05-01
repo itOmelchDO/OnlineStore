586f0adc3c830220e922f402299a3274d2f1555d
itOmelchDO <it.omelchenko@gmail.com>
2023-05-01 23:51:26 +0300

    'Added OrderCreateView in orders app'

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,12 @@
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
+from common.views import CommonMixin
 from orders.forms import OrderForm
 
 
-class OrderCreateView(CreateView):
+class OrderCreateView(CommonMixin, CreateView):
     template_name = "orders/order-create.html"
     form_class = OrderForm
+    success_url = reverse_lazy("orders:order_create")
+    title = "DESIREX - Оформлення замовлення"
