f2b9c443d9ff330f076e7a94cbc1f503e94cfda7
itOmelchDO <it.omelchenko@gmail.com>
2023-05-04 14:54:08 +0300

    'Added caching products with redis'

--- OnlineStore/settings.py
+++ OnlineStore/settings.py
@@ -94,6 +94,13 @@ INTERNAL_IPS = [
     "localhost",
 ]
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
