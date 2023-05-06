97666f3e0e6ff8e694613e383370e9bd4a8f28ae
itOmelchDO <it.omelchenko@gmail.com>
2023-05-07 00:40:14 +0300

    'Added Basket API'

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from api.views import ProductModelViewSET
+from api.views import ProductModelViewSET, BasketModelViewSet
 
 router = routers.DefaultRouter()
 router.register(r"products", ProductModelViewSET)
+router.register(r"basket", BasketModelViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
